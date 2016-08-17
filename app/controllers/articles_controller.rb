d70343dd4a99e001bdc393805f608a0edb135903
root <root@localhost.localdomain>
2016-08-17 17:19:19 -0400

    Add partials and destory actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -49,6 +49,14 @@ class ArticlesController < ApplicationController
 	end
 
 
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		redirect_to articles_path
+		flash[:notice] = "Article was successfully deleted"
+	end
+
+
 
 private
 	def article_params
