d6e46471cc1fd733a91561e7d71110747cfeb9a0
root <root@localhost.localdomain>
2016-08-22 21:29:46 -0400

    Updating the alpha blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,15 @@
 class ArticlesController < ApplicationController
+before_action :set_article, only: [:edit, :update, :show, :destroy]
 
 	def index
 		@articles = Article.all
-
-
 	end
 
 	def new
 		@article=Article.new
-
 	end
 
 	def edit
-		@article = Article.find(params[:id])
-
-
 	end
 
 	def create
@@ -33,7 +28,7 @@ class ArticlesController < ApplicationController
 	end
 
 	def update
-		@article = Article.find(params[:id])
+
 		if @article.update(article_params)
 			flash[:notice] = "Article was successfully updated"
 			redirect_to article_path(@article)
@@ -44,13 +39,13 @@ class ArticlesController < ApplicationController
 
 
 	def show
-		@article = Article.find(params[:id])
+
 
 	end
 
 
 	def destroy
-		@article = Article.find(params[:id])
+
 		@article.destroy
 		redirect_to articles_path
 		flash[:notice] = "Article was successfully deleted"
@@ -59,10 +54,11 @@ class ArticlesController < ApplicationController
 
 
 private
+def set_article
+	@article = Article.find(params[:id])
+end
 	def article_params
-
 		params.require(:article).permit(:title, :description)
-
 	end
 
 
